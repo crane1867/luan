5e256061b1089b49a81e3971c8f719930678e453
crane1867 <crane_moretti@outlook.com>
2025-05-05 21:45:32 +0800

    Update uninstall_let_bot.sh

--- uninstall_let_bot.sh
+++ uninstall_let_bot.sh
@@ -7,10 +7,10 @@ crontab -l | grep -v "let_offers_bot.py" | crontab -
 echo "[*] 删除工作目录 /root/let_bot ..."
 rm -rf /root/let_bot
 
-echo "[*] （可选）卸载系统依赖 python3-venv, python3-pip, curl ..."
+echo "[*] （可选）卸载系统依赖 python3-venv, python3-pip ..."
 read -p "是否同时卸载系统依赖？[y/N] " yn
 if [[ $yn =~ ^[Yy]$ ]]; then
-  apt remove --purge -y python3-venv python3-pip curl
+  apt remove --purge -y python3-venv python3-pip
   apt autoremove -y
 fi
 
